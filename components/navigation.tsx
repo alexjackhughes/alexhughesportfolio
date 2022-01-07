4277c7f5ab43276df73a964edfeb5185c55f3179
Alex <alex@alexjackhughes.com>
2022-01-07 17:20:37 +0000

    Add styles to navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,51 +3,91 @@ import Image from "next/image";
 import { useState } from "react";
 
 const Naivgation = () => {
-  const [showBlink, setShowBlink] = useState(false);
-
   return (
-    <header className="sticky top-0 z-50 bg-white py-3">
+    <header
+      style={{
+        background: "rgba(255, 255, 255, 0.8)",
+      }}
+      className="sticky top-0 z-50 py-3"
+    >
       <div className="flex flex-col md:flex-row justify-between items-center container mx-auto px-5">
-        {showBlink ? (
-          <Image
-            onClick={() => {
-              setShowBlink(false);
-            }}
-            src="/logo/Alex Hughes | Logo - Blink.svg"
-            className="cursor-pointer bounce"
-            height={80}
-            width={80}
-          />
-        ) : (
-          <Image
-            onClick={() => {
-              setShowBlink(true);
-            }}
-            src="/logo/Alex Hughes | Logo.svg"
-            className="cursor-pointer"
-            height={80}
-            width={80}
-          />
-        )}
-        <div className="hidden md:flex">
-          <Link href="/">
-            <a className="hover:underline">Home</a>
-          </Link>
-          <Link href="/">
-            <a className="hover:underline">Blog</a>
-          </Link>
-        </div>
-        <div>
-          <Link href="/">
-            <a className="hover:underline">Home</a>
-          </Link>
-          <Link href="/">
-            <a className="hover:underline">Blog</a>
-          </Link>
-        </div>
+        <Logo />
+        <SocialLinks />
+        <NavLinks />
       </div>
     </header>
   );
 };
+
+const Logo = () => {
+  const [showBlink, setShowBlink] = useState(false);
+
+  return (
+    <>
+      {showBlink ? (
+        <Image
+          onClick={() => {
+            setShowBlink(false);
+          }}
+          src="/logo/Alex Hughes | Logo - Blink.svg"
+          className="cursor-pointer bounce"
+          height={80}
+          width={80}
+        />
+      ) : (
+        <Image
+          onClick={() => {
+            setShowBlink(true);
+          }}
+          src="/logo/Alex Hughes | Logo.svg"
+          className="cursor-pointer"
+          height={80}
+          width={80}
+        />
+      )}
+    </>
+  );
+};
+
+const SocialLinks: React.FC = () => (
+  <div className="hidden md:flex">
+    <a
+      target="_blank"
+      rel="noreferrer noopener"
+      href="https://twitter.com/alexjackhughes"
+    >
+      <LinkText>Twitter</LinkText>
+    </a>
+    <a
+      target="_blank"
+      rel="noreferrer noopener"
+      href="https://github.com/alexjackhughes"
+    >
+      <LinkText>Github</LinkText>
+    </a>
+    <a
+      target="_blank"
+      rel="noreferrer noopener"
+      href="https://www.linkedin.com/in/alexjackhughes/"
+    >
+      <LinkText>LinkedIn</LinkText>
+    </a>
+  </div>
+);
+
+const NavLinks: React.FC = () => (
+  <div>
+    <Link href="/">
+      <LinkText>Home</LinkText>
+    </Link>
+    <Link href="/">
+      <LinkText>Blog</LinkText>
+    </Link>
+  </div>
+);
+
+const LinkText: React.FC = ({ children }) => (
+  <a className="hover:underline">{children}</a>
+);
 
 export default Naivgation;
