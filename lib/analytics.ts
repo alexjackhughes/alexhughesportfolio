675af092aea062f1e4bae3ea5f6a48f6ea6e6519
Alex <alex@alexjackhughes.com>
2022-01-11 01:23:22 +0000

    Add google analytics

---
+++ lib/analytics.ts
@@ -0,0 +1,13 @@
+// @ts-nocheck
+
+// log the pageview with their URL
+export const pageview = (url: any) => {
+  window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+    page_path: url,
+  });
+};
+
+// log specific events happening.
+export const event = ({ action, params }) => {
+  window.gtag("event", action, params);
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,6 +23,22 @@ export default class MyDocument extends Document {
             property="og:image"
             content="https://alexhughes.io/template.png"
           />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
