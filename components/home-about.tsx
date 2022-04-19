f078024c09f898ba8408ba0c7404c423810bb384
Alex <alex@alexjackhughes.com>
2022-04-19 17:02:59 +0100

    Improve copy

--- components/home-about.tsx
+++ components/home-about.tsx
@@ -6,44 +6,50 @@ const HomeAbout: React.FC = () => {
       <div className="bg-sky-800 py-20">
         <Container>
           <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold md:leading-none text-center md:text-left text-sky-100 mb-10">
-            About me
+            Who am I?
           </h1>
           <div className=" lg:w-4/5">
             <p className="text-sky-100 text-xl mb-5">
-              My name is Alexander Jack Hughes, but you can call me Alex. I'm a
-              full-stack engineer and builder of high-growth startups. To give
-              you a quick history lesson; I've spent the last decade tinkering
-              away, completing a MSc in Computer Science, and writing a few
-              things for Hackernoon.
+              I'm the CTO of Loopin. When I'm not busy building our full-stack
+              app or growing our team, I spend my days tinkering with AI art
+              projects and writing for Hackernoon.
             </p>
             <p className="text-sky-100 text-xl mb-5">
-              I can name drop if you'd like?
+              Would you like a quick history lesson? Sure! I've spent the last
+              decade coding in the startup space, in every position from junior
+              developer to CTO. I've had the pleasure to work for some of the
+              most interesting startups in the South West and London.
             </p>
+            <p className="text-sky-100 text-xl mb-5">Which startups you ask?</p>
             <p className="text-sky-100 text-xl mb-5">
               Right now, I'm the CTO of{" "}
               <Link href="https://letsloopin.com">
                 <a>Loopin</a>
               </Link>
-              - a people analytics platform that's the future of how you build
-              good company culture. Before that I worked at a couple of pretty
-              successful startups,{" "}
+              . I joined when it was two marines and a tin shed - and now, we
+              have one of the most interesting people analytics platforms in the
+              world.
+            </p>
+            <p className="text-sky-100 text-xl mb-5">
+              Before that, I was building{" "}
               <Link href="https://tumelo.com">
                 <a>Tumelo</a>
               </Link>{" "}
-              and{" "}
+              helping to bring transparency to where your money goes. And before
+              that, I was the 2nd dev hire at{" "}
               <Link href="https://www.huckletree.com">
                 <a>Huckletree</a>
               </Link>{" "}
-              included. And before <span className="italic">that</span>, I
-              designed the UI/UX for{" "}
+              (one of Europe's fastest growing workspaces). And before{" "}
+              <span className="italic">that</span>, I designed the UI/UX for{" "}
               <Link href="https://wordpress.org/plugins/updraftplus/">
                 <a>UpdraftPlus</a>
               </Link>{" "}
-              - which, with more than 3 million downloads is one of the most
+              . Which, with over 3 million downloads, is one of the most
               downloaded plugins on WordPress.
             </p>
-            <p className="text-sky-100 text-xl">
-              But enough about me, how have you been?
+            <p className="text-sky-100 text-xl mb-5">
+              But enough about me. How have you been?
             </p>
           </div>
         </Container>
--- components/home-splash.tsx
+++ components/home-splash.tsx
@@ -1,4 +1,16 @@
 const HomeSplash: React.FC = () => {
+  // Hey you! Yes you, reading this - feel free to submit a PR with other ideas! 🙏
+  const phrases = [
+    "all round great guy",
+    "king of the north",
+    "builder of high-growth startups",
+    "passionate about startups",
+    "Typescript Connoisseur",
+    "dabbler in the dark arts",
+    "wannabe AI artist",
+    "React whisperer",
+  ];
+
   return (
     <div className="h-full w-screen flex flex-col bg-sky-600 justify-between">
       <div className="my-32 md:mt-64 container mx-auto px-5 xl:px-64">
@@ -6,7 +18,10 @@ const HomeSplash: React.FC = () => {
           Hi, I'm <span className="skittles">Alex</span>.
         </h1>
         <div className="poppins text-sky-100 text-center md:text-left mt-6 text-xl">
-          <p className="mb-1">CTO of Loopin and king of the north</p>
+          <p className="mb-1">
+            CTO of Loopin and{" "}
+            {phrases[Math.floor(Math.random() * phrases.length)]}
+          </p>
         </div>
       </div>
       <img src="/waves/home.svg" className="w-screen" />
